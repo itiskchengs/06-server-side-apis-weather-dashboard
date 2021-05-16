7ed0b26f1153d0f1c3837531a6e768e6544a67fa
Kenneth Cheng <itskcheng@gmail.com>
2021-05-15 23:15:18 -0400

    Finshed up with adding functionality to the weather dashboard and added to the readme file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # 06-server-side-apis-weather-dashboard
+
+In this homework I build a weather dashboard using the open weather api. This week there was no code files given to us so I created everything from scratch. I created the HTML / CSS and JavaScript. I used bootstrap to help with the layout of the page. Styled the page with CSS and created the functionality with JavaScript. This weather dashboard lets you search for a city and it will display the weather, temp, humidity and uv index. It also will give you the forecast for the next five days. Also it saves a button everytime for you to go back to that city if you would like to do so. 
+
+- Created the HTML, CSS and JavaScript files
+
+- Used the openweatherAPI to gather data when a user searches a city then you get JSON data back that you are able to use and display on the page.  
+
+- Used local storage to save the city so it can be used back on the buttons when they are click to display the data of that saved city.
+
+# Screenshot
+<br>
+
+![This is a weather dashboard where you can search a city in the input field and when you submit. weather data is displayed with the temperature, humidity, wind, uv index. Also the five day forecast is given and a button that you can go back to a city you previously searched.](assets/img/weatherapi.png)
+
+<br>
+
+# Deployment Link
+
+Below is the deployment github pages link.
+
+[06-homework-deployment-link](https://itiskchengs.github.io/06-server-side-apis-weather-dashboard/)
--- assets/script.js
+++ assets/script.js
@@ -4,74 +4,185 @@ var searchInputBtn = document.querySelector('#city-submit');
 var searchHistoryContainer = document.querySelector('.search-history-section');
 var cityResult = document.querySelector('.city-result');
 var fiveDayResult = document.querySelector('.five-day-result');
+var cityButton = document.querySelector('.city-button');
+var cities = JSON.parse(localStorage.getItem('cities')) || [];
 
+//Moment JS
+var currentDate = moment();
+var nextFiveDays = moment().add(5, 'd');
 
 //Set event listerners to the query selector elements 
 searchInputBtn.addEventListener('click', searchCity);
+searchHistoryContainer.addEventListener('click', searchCity);
 
-//API url and key
+//Global Variables for the search display
+var searchResultHeading = document.createElement('h2');
+var searchParagraphTemp = document.createElement('p');
+var searchParagraphWind = document.createElement('p');
+var searchParagraphHumidity = document.createElement('p');
 
 //Create the fetch function to fetch data from weather API
 function searchCity(e) {
     e.preventDefault;
-    var city = searchInputValue.value;
+    console.log(e);
+    if(e.target.id === 'city-submit'){
+        var city = searchInputValue.value;
+    }else{
+        var city = e.target.textContent;
+    }
     var currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=5ef304080ee3c4f774ac95eb5d38536f`;
+
+    var forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&cnt=5&appid=5ef304080ee3c4f774ac95eb5d38536f`;
+
+    //Fetch current weather
     fetch(currentWeather)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             displayWeather(data);
-            console.log(data)
+            uvIndexFetch(data);
+            if(e.target.id === 'city-submit'){
+                setLocalStorage(data);
+                allCities();
+            }
+            console.log(data);
         })
+
+    //Fetch forecast
+    fetch(forecast)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            fiveDayForecast(data);
+            console.log(data);
+        })
+
+    //Fetch UV Index
+    var uvIndexFetch = function (data) {
+        var lon = data.coord.lon;
+        var lat = data.coord.lat;
+
+        var uvIndexUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=5ef304080ee3c4f774ac95eb5d38536f`;
+
+        fetch(uvIndexUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                displayUvi(data);
+                console.log(data);
+            })
+    }
 }
-
-var displayWeather = function (data){
-    var searchResultHeading = document.createElement('h2');
-    var searchParagraphTemp = document.createElement('p');
-    var searchParagraphWind = document.createElement('p');
-    var searchParagraphHumidity = document.createElement('p');
-    var searchParagraphUvIndex = document.createElement('p');
-    searchResultHeading.textContent = data.name;
-    searchParagraphTemp.textContent = data.main.temp + ' °F';
-    searchParagraphWind.textContent = data.wind.speed + ' MPH';
-    searchParagraphHumidity.textContent = data.main.humidity + ' %';
-
-    var valueObject = [{
-        cityName: searchResultHeading.textContent,
-        cityTemp: searchParagraphTemp.textContent,
-        cityWind: searchParagraphWind.textContent,
-        cityHumidity: searchParagraphHumidity.textContent
-    }]
-
-    localStorage.setItem(searchResultHeading.textContent, JSON.stringify(valueObject));
-
-    cityResult.append(searchResultHeading);
-    searchResultHeading.append(searchParagraphTemp);
-    searchParagraphTemp.append(searchParagraphWind);
-    searchParagraphWind.append(searchParagraphHumidity);
-    
-}
-
 
 //Create the search results function 
-//Display city, date
-//Display temp
-//Display wind
-//Display Humidity
-//Display uv index
 
-//Create function that displays a five dat forecast
-//Use a for loop to loop through the data for the five day forecast
-//Create the elements needed to display the forecast
-//Display Background
-//Display weather emote
-//Display Tempeature
-//Display Wind Temp
-//Display Humidity
-//Append the data
+var displayWeather = function (data) {
+    //Resets the city results container everytime the function runs
+    cityResult.innerHTML = '';
 
-//Create a function that when you search for the data its saved to local storage and builds a button so it can be clicked later to get the data back.
-//Create button
-//Add text to button 
-//Append it to the div container
+    //Adding classes to the HTML elements
+    searchResultHeading.classList.add('city-result-heading');
+    searchParagraphTemp.classList.add('city-result-paragraph');
+    searchParagraphWind.classList.add('city-result-paragraph');
+    searchParagraphHumidity.classList.add('city-result-paragraph');
+
+    /*WeatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);*/
+
+    //add api data to the text content of the HTMl elements
+    searchResultHeading.textContent = data.name + ' ' + currentDate.format('l');
+    searchParagraphTemp.textContent = 'Temp: ' + data.main.temp + ' °F';
+    searchParagraphWind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
+    searchParagraphHumidity.textContent = 'Humidity: ' + data.main.humidity + ' %';
+
+    //Apending the elements to the div container
+    cityResult.append(searchResultHeading);
+    cityResult.append(searchParagraphTemp);
+    cityResult.append(searchParagraphWind);
+    cityResult.append(searchParagraphHumidity);
+}
+
+//Create the function that display the uv index
+var displayUvi = function (data) {
+    //Creates the paragraph element
+    var searchParagraphUvIndex = document.createElement('p');
+    //Add the class to the element
+    searchParagraphUvIndex.classList.add('city-result-paragraph', 'uv-color');
+    //Change the text content to the api data
+    searchParagraphUvIndex.textContent = 'UV Index: ' + data.current.uvi;
+    //Apend the paragraph element to the div container
+    cityResult.append(searchParagraphUvIndex);
+}
+
+//Create a function that when you search for the data its saved to local storage
+var setLocalStorage = function (data) {
+    //create the variable value object that is set to an array of the data.name which is the city name
+    var valueObject = [data.name];
+    //take the data from value object variable which is the city name and push it into the cities variable 
+    cities.push(valueObject);
+    console.log(cities);
+    //Set the cities variables to local storage
+    localStorage.setItem('cities', JSON.stringify(cities));
+
+}
+
+//This function works when you search a city it creates a button that saves the city so you can be able to click back on the button afterwards to get the data back to be displayed
+var allCities = function() {
+    searchHistoryContainer.innerHTML = '';
+    for(var i = 0; i < cities.length; i++){
+
+        var searchHistoryButton = document.createElement('div');
+        searchHistoryButton.innerHTML = `
+        <button class="city-button">${cities[i]}</button>
+        `
+        searchHistoryContainer.append(searchHistoryButton);
+    }
+}
+
+
+//Create function that displays a five day forecast
+
+var fiveDayForecast = function (data) {
+    fiveDayResult.innerHTML = '';
+    //Create the elements
+    var fiveDayHeadingContainer = document.createElement('div');
+    var fiveDayHeading = document.createElement('h2');
+    fiveDayHeadingContainer.classList.add('col-xl-12', 'col-lg-12', 'col-md-12');
+    fiveDayHeading.textContent = '5-Day Forecast:';
+    fiveDayResult.append(fiveDayHeadingContainer);
+    fiveDayHeadingContainer.append(fiveDayHeading);
+
+    //Use a for loop to loop through the data for the five day forecast
+    for (var i = 0; i < data.list.length; i++) {
+        console.log(data.list[i]);
+        var fiveDayForecastContainer = document.createElement('div');
+        var fiveDayDate = document.createElement('p');
+        var fiveDayWeatherIcon = document.createElement('img');
+        var fiveDayForecastTemp = document.createElement('p');
+        var fiveDayForecastWind = document.createElement('p');
+        var fiveDayForecastHumidity = document.createElement('p');
+
+        //Adding classes and setting attributes
+        fiveDayForecastContainer.classList.add('col-xl-2', 'col-lg-2', 'col-md-2', 'forecast-container');
+        fiveDayWeatherIcon.classList.add('forecast-image');
+        fiveDayDate.textContent = nextFiveDays.format('l');
+        fiveDayWeatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`);
+
+        //adding the textcontent in the elements
+        fiveDayForecastTemp.textContent = 'Temp: ' + data.list[i].main.temp + ' °F';
+        fiveDayForecastWind.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
+        fiveDayForecastHumidity.textContent = 'Humidity: ' + data.list[i].main.humidity + ' %';
+
+        //Appending the variables to the page
+        fiveDayResult.append(fiveDayForecastContainer);
+        fiveDayForecastContainer.append(fiveDayDate);
+        fiveDayForecastContainer.append(fiveDayWeatherIcon);
+        fiveDayForecastContainer.append(fiveDayForecastTemp);
+        fiveDayForecastContainer.append(fiveDayForecastWind);
+        fiveDayForecastContainer.append(fiveDayForecastHumidity);
+    }
+}
+
+allCities();
